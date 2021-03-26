1eaeeba23fb36282483fa3b1b8ad2d47c7999be8
abouhanifa <abdellah.abouhanifa@gmail.com>
2021-03-26 01:19:21 +0100

    commitS

---
+++ project_mern_memories/client/build/precache-manifest.ab564c1ad98bfb68755849cc5fe08dc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f144605b76f83be3a3be86d43f7f6837",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a23d2a8f0812cc315e2",
+    "url": "/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a23d2a8f0812cc315e2",
+    "url": "/static/js/main.fa5a69f4.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
